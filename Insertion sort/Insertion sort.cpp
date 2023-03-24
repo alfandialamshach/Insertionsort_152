710ab916dd34cf7160dd6c73675f2ef3d901bfe3
alfandi <fandidixpc1@gmail.com>
2023-03-24 14:54:13 +0700

    Membuat tampilan inputan jumlah elemen array dan perintah untuk memanggil variabel inputan n

--- Insertion sort/Insertion sort.cpp
+++ Insertion sort/Insertion sort.cpp
@@ -6,4 +6,9 @@ int n;					// Membuat Variable inputan n
 
 void input() {			// Procedure input
 	while (true)
+	{
+		cout << "Masukkan jumlah data pada array : "; // Membuat inputan jumlah elemen array
+		cin >> n;									// Memanggil variable inputan n
+
+	}
 }
