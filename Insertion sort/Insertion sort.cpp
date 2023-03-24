240ec55932457ee1f8ee1a04a120a30c68556537
alfandi <fandidixpc1@gmail.com>
2023-03-24 16:12:01 +0700

    Menambahkan ; pada ln4  setelah [20]

--- Insertion sort/Insertion sort.cpp
+++ Insertion sort/Insertion sort.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int arr[20]				// Membuat Array dengan panjang 20
+int arr[20];			// Membuat Array dengan panjang 20
 int n;					// Membuat Variable inputan n
 
 void input() {			// Procedure input
