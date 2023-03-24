617965dc664daa4f5bf12c5cc017e066e591ffee
alfandi <fandidixpc1@gmail.com>
2023-03-24 14:59:50 +0700

    Menambahkan tampilan, jika data lebih dari 20 maka akan keluar pesan Array yang anda masukkan maksimal 20 elemen.

--- Insertion sort/Insertion sort.cpp
+++ Insertion sort/Insertion sort.cpp
@@ -13,5 +13,9 @@ void input() {			// Procedure input
 		if (n <= 20) {								// Membuat kondisi n tidak lebih dari 20
 			break;
 		}
+		else
+		{
+			cout << "\nArray yang anda masukkan maksimal 20 elemen.\n"; // Menampilkan pesan jika data lebih dari 20
+		}
 	}
 }
